9eca940c4103f42ab76d56e5894298e44d06500d
TUFnarayan <125310038+TUFnarayan@users.noreply.github.com>
2024-04-28 12:32:56 +0000

    container config done

---
+++ basics/dcon.js
@@ -0,0 +1,9 @@
+let score=33;
+console.log(typeof score);
+let scoree="33abc";
+let value=Number(scoree);
+console.log(typeof value);
+console.log(value);
+let isnul=null;
+let vv=Number(isnul);
+console.log(vv);
